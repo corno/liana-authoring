1d7ad23684f48dea8d60e3749ec0c5ecc14c7a9a
Corno Schraverus <corno@schraverus.com>
2026-05-02 21:04:52 +0200

    pdt: published version 0.1.27

--- typescript/lib/src/implementation/manual/transformers/deserialize/diagnostics.ts
+++ typescript/lib/src/implementation/manual/transformers/deserialize/diagnostics.ts
@@ -8,6 +8,8 @@ import * as d_out from "../../../../interface/generated/liana/schemas/diagnostic
 //depencencies
 import * as t_get_schema from "../get_schema/diagnostics"
 import * as t_deserialize_to_location from "astn-core/dist/implementation/manual/transformers/deserialize_parse_tree/location"
+import * as t_deserialize_to_fp from "astn-core/dist/implementation/manual/transformers/deserialize_parse_tree/fountain_pen"
+import * as t_fp_to_text from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/text"
 
 export const Error: _pi.Transformer<d_in.Error, d_out.Diagnostics.L> = ($) => {
 	return _p.decide.state($, ($) => {
@@ -22,7 +24,13 @@ export const Error: _pi.Transformer<d_in.Error, d_out.Diagnostics.L> = ($) => {
 			case 'schema': return _p.ss($, ($) => t_get_schema.Error($))
 			case 'deserialize': return _p.ss($, ($) => ({
 				'severity': ['error', null],
-				'message': `Failed to deserialize (FIXME)`,
+				'message': t_fp_to_text.Phrase(
+					t_deserialize_to_fp.Error($),
+					{
+						'indentation': "    ",
+						'newline': "\n",
+					}
+				),
 				'range': _p.optional.literal.set(t_deserialize_to_location.Error($)),
 				'related information': _p.optional.literal.not_set(),
 				'type': ['deserialize', null]
