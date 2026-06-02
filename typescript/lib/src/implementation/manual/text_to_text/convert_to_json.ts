f481dcdf44ba6d8c9d054122ae264f9243a06d6a
Corno Schraverus <corno@schraverus.com>
2026-06-02 12:03:23 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/text_to_text/convert_to_json.ts
+++ typescript/lib/src/implementation/manual/text_to_text/convert_to_json.ts
@@ -10,7 +10,7 @@ type Signature = _pi.Refiner_With_Parameter<d_out.Text, d_function_deserialize_p
 
 //dependencies
 import * as t_ast_2_json from "astn/dist/implementation/manual/transformers/parse_tree/json_target"
-import * as t_json_to_text from "pareto-json/dist/implementation/manual/transformers/json/text"
+import * as t_json_to_text from "pareto-json/dist/implementation/manual/transformers/json_without_guaranteed_unique_keys/text"
 import * as r_astn_parse_tree_from_text from "astn-core/dist/implementation/manual/refiners/parse_tree/text"
 
 
