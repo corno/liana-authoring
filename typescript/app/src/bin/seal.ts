fa8236a03303e0150f9d94fb57b63f2ec95068f3
Corno Schraverus <corno@schraverus.com>
2026-06-08 17:29:41 +0200

    pdt: ...

--- typescript/app/src/bin/seal.ts
+++ typescript/app/src/bin/seal.ts
@@ -7,12 +7,12 @@ import { $$ as procedure } from "lib/dist/implementation/manual/commands/seal"
 _pn.run_main_command(
     ($r) => procedure(
         {
-            'write file': $r.commands['write file'],
-            'log error': $r.commands['log error'],
+            'write file': $r['filesystem unrestricted'].commands['write file'](null),
+            'log error': $r.stream.commands['log error'](null),
         },
         {
-            'read file': $r.queries['read file'],
-            'stat': $r.queries['stat possible node']
+            'read file': $r['filesystem unrestricted'].queries['read file'](null),
+            'stat': $r['filesystem unrestricted'].queries['stat possible node'](null)
         },
     ),
 )
--- typescript/lib/src/implementation/manual/queries/load_unmarshalled_file.ts
+++ typescript/lib/src/implementation/manual/queries/load_unmarshalled_file.ts
@@ -10,8 +10,8 @@ import * as d from "../../../interface/to_be_generated/get_unmarshalled_file"
 
 //depencencies
 import * as r_unmarshall_result_from_list_of_characters from "../refiners/unmarshall_result/list_of_characters"
-import * as t_path_to_text from "pareto-resources/dist/implementation/manual/transformers/path/text"
-import * as t_path_to_path from "pareto-resources/dist/implementation/manual/transformers/path/path"
+import * as t_path_to_text from "pareto-resources/dist/implementation/manual/transformers/unrestricted_path/text"
+import * as t_path_to_path from "pareto-resources/dist/implementation/manual/transformers/unrestricted_path/unrestricted_path"
 import * as r_temp_module_specifier_from_loc from "pareto-liana/dist/implementation/manual/refiners/temp_module_specifier/list_of_characters"
 import { $$ as q_deserialize } from "./deserialize"
 import { $$ as q_get_schema } from "./get_schema"
